c2448001e0ab2b470961c6fd4cfeac8efdfa305d
CAROLINE WANJIKU MAINA <carolinemaina440@gmail.com>
2026-09-21 22:10:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ JOB DESCRIPTION:
             # ----------------------------------
 
             response = client.models.generate_content(
-                model="gemini-3.6-flash",
+                model="gemini-2.5-flash",
                 contents=prompt
             )
 
